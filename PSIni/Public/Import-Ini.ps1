f7f3475c93d642ece7bdfe1d7e604d441dffadb1
HighPriest <git_user@amun.pl>
2024-10-09 01:40:29 +0200

    [Fix] Reading empty lines as key, value pairs
    If an empty line is detected, the line is discarded
    Also adds Trim of white characters from keys

--- PSIni/Public/Import-Ini.ps1
+++ PSIni/Public/Import-Ini.ps1
@@ -87,6 +87,7 @@ function Import-Ini {
 
             if (-not (Test-Path -Path $file)) {
                 Write-Error "Could not find file '$file'"
+                continue
             }
 
             $commentCount = 0
@@ -125,17 +126,19 @@ function Import-Ini {
                         $ini[$section] = New-Object System.Collections.Specialized.OrderedDictionary([System.StringComparer]::OrdinalIgnoreCase)
                     }
                     $name, $value = $matches[1].Trim(), $matches[3].Trim()
-                    Write-Verbose "$($MyInvocation.MyCommand.Name):: Adding key $name with value: $value"
-                    if (-not $ini[$section][$name]) {
-                        $ini[$section][$name] = $value
-                    }
-                    else {
-                        if ($ini[$section][$name] -is [string]) {
-                            $oldValue = $ini[$section][$name]
-                            $ini[$section][$name] = [System.Collections.ArrayList]::new()
-                            $null = $ini[$section][$name].Add($oldValue)
+                    if (-not [string]::IsNullOrWhiteSpace($name)) {
+                        Write-Verbose "$($MyInvocation.MyCommand.Name):: Adding key $name with value: $value"
+                        if (-not $ini[$section][$name]) {
+                            $ini[$section][$name] = $value
                         }
-                        $null = $ini[$section][$name].Add($value)
+                        else {
+                            if ($ini[$section][$name] -is [string]) {
+                                $oldValue = $ini[$section][$name]
+                                $ini[$section][$name] = [System.Collections.ArrayList]::new()
+                                $null = $ini[$section][$name].Add($oldValue)
+                            }
+                            $null = $ini[$section][$name].Add($value)
+                        }
                     }
                     continue
                 }
@@ -147,9 +150,11 @@ function Import-Ini {
                         $section = $script:NoSection
                         $ini[$section] = New-Object System.Collections.Specialized.OrderedDictionary([System.StringComparer]::OrdinalIgnoreCase)
                     }
-                    $name = $_
-                    Write-Verbose "$($MyInvocation.MyCommand.Name):: Adding key $name without a value"
-                    $ini[$section][$name] = $null
+                    $name = $_.Trim()
+                    if (-not [string]::IsNullOrWhiteSpace($name)) {
+                        Write-Verbose "$($MyInvocation.MyCommand.Name):: Adding key $name without a value"
+                        $ini[$section][$name] = $null
+                    }
                     continue
                 }
             }
