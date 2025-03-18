b05b1f555728ea788aa7d6ee15496d3b712a301c
HighPriest <git_user@amun.pl>
2025-03-18 20:17:21 +0100

    Tag Remove-EmptyLine as obsolete

--- PSIni/Private/Remove-EmptyLine.ps1
+++ PSIni/Private/Remove-EmptyLine.ps1
@@ -17,6 +17,8 @@
     $lf = if ((-not $PSVersionTable.Platform) -or ($PSVersionTable.Platform -eq "Win32NT")) { "\r\n" }
     else { "\n" }
 
-    $content = (Get-Content -Path $Path -ErrorAction SilentlyContinue -Raw) -replace "(${lf})*$", "" -replace "(${lf}){3,}", ""
+    # TODO: Verify if this code is actually necessary & purge from the codebase, if not.
+    # Makes sure that there are no more than singular empty lines in entire file & that the file ends without empty lines
+    $content = (Get-Content -Path $Path -Raw) -replace "(${lf})*$", "" -replace "(${lf}){3,}", ""
     Set-Content -Value $content -Path $Path -Encoding $Encoding -Force:$Force
 }
