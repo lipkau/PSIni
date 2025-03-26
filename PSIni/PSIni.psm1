d3e702a3050de81c27459b00e7560614ac612fcd
Oliver Lipkau <4992870+lipkau@users.noreply.github.com>
2025-03-26 11:05:20 +0100

    fixed file case which is breaking the ci pipeline (#91)

---
+++ PSIni/PSIni.psm1
@@ -0,0 +1,27 @@
+﻿<#
+    .Synopsis
+        This Module contains functions to manage INI files
+
+    .Notes
+        Author       : Oliver Lipkau <https://github.com/lipkau>
+        Contributors : https://github.com/lipkau/PSIni/graphs/contributors
+        Homepage     : http://lipkau.github.io/PSIni/
+
+#>
+
+#region Configuration
+# Name of the Section, in case the ini file had none
+# Available in the scope of the module as `$script:NoSection`
+$script:NoSection = "_"
+#endregion Configuration
+
+#region LoadFunctions
+$PublicFunctions = @( Get-ChildItem -Path "$PSScriptRoot/Public/*.ps1" -ErrorAction SilentlyContinue )
+$PrivateFunctions = @( Get-ChildItem -Path "$PSScriptRoot/Private/*.ps1" -ErrorAction SilentlyContinue )
+
+# Dot source the functions
+foreach ($file in @($PublicFunctions + $PrivateFunctions)) {
+    . $file.FullName
+}
+Export-ModuleMember -Function $PublicFunctions.BaseName -Alias *
+#endregion LoadFunctions
