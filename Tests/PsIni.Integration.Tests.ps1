ed4b1648e8db09ddf71c927ac80f0ada3f18272a
Oliver Lipkau <oliver@lipkau.net>
2025-05-11 22:56:01 +0200

    Added `-InputString` to `Import-Ini`
    This allows a string representation of a ini file to be imported.
    This can be useful when the file is read from a web request.
    
    In order to implement this, a slight performance buff was implemented:
    files are now read with `[System.IO.File]::ReadAllLines()` instead for `switch -file`
    Read more: https://devdojo.com/hcritter/powershell-performance-test-file-reading
    
    closes #62, closes #71

--- Tests/PsIni.Integration.Tests.ps1
+++ Tests/PsIni.Integration.Tests.ps1
@@ -8,6 +8,8 @@ Describe "PSIni integration tests" -Tag "Integration" {
         Remove-Module PSIni -ErrorAction SilentlyContinue
         Import-Module $moduleToTest -Force -ErrorAction Stop
 
+        $script:tempFolder = [System.IO.Path]::GetTempPath()
+
         $dictIn = New-Object System.Collections.Specialized.OrderedDictionary([System.StringComparer]::OrdinalIgnoreCase)
         $dictIn["Category1"] = New-Object System.Collections.Specialized.OrderedDictionary([System.StringComparer]::OrdinalIgnoreCase)
         $dictIn["Category1"]["Key1"] = "Value1"
@@ -17,8 +19,11 @@ Describe "PSIni integration tests" -Tag "Integration" {
         $dictIn["Category2"]["Key4"] = "Value4"
     }
     BeforeEach {
-        Export-Ini -InputObject $dictIn -Path "TestDrive:\output.ini" -Force -ErrorAction Stop
-        $script:dictOut = Import-Ini -Path "TestDrive:\output.ini" -ErrorAction Stop
+        Export-Ini -InputObject $dictIn -Path "$tempFolder/output.ini" -Force -ErrorAction Stop
+        $script:dictOut = Import-Ini -Path "$tempFolder/output.ini" -ErrorAction Stop
+    }
+    AfterAll {
+        Remove-Item "$tempFolder/output.ini"
     }
 
     It "content matches original hashtable" {
